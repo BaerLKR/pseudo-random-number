ff97fd4b54055cd1ebc7ed3e32f96005e2e949be
Lovis Rentsch <gpg@lovirent.eu>
2023-06-05 23:30:48 +0200

    custom sine function

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,21 @@
 fn main() {
     println!("{}", pseudorandom())
 }
+const PI: f64 = std::f64::consts::PI;
 fn pseudorandom() -> i32 {
     let out: i32;
     use std::time::{SystemTime, UNIX_EPOCH};
     match SystemTime::now().duration_since(UNIX_EPOCH) {
         Ok(n) => {
-            let n = n.as_micros();
-            // println!("{n}");
-            let n = n as f64;
-            let n = n.tan().sin();
-            let n = n * 100000000.0;
-            // let n = n.abs();
-            let n = format!("{:.0}", n);
-            // println!("{n}");
+            let n = n.as_micros() as f64;
+            // let n = format!("{:.0}", n.tan().sin() * 100000000.0);
+            let n = format!("{:.0}", custom_sin(n, 340763470.0) * 1000000000.0);
             out = n.trim().parse().unwrap();
         }
         Err(_) => panic!("SystemTime before UNIX EPOCH!"),
     }
     out
+}
+fn custom_sin(x: f64, frequency: f64) -> f64 {
+    (frequency * x * 2.0 * PI).sin()
 }
