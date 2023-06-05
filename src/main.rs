370fe084b82360231d3ee9cb2e493c38aa7f961d
Lovis Rentsch <gpg@lovirent.eu>
2023-06-05 23:20:42 +0200

    made it a return function, that returns a number

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,22 @@
 fn main() {
+    println!("{}", pseudorandom())
+}
+fn pseudorandom() -> i32 {
+    let out: i32;
     use std::time::{SystemTime, UNIX_EPOCH};
-
     match SystemTime::now().duration_since(UNIX_EPOCH) {
         Ok(n) => {
             let n = n.as_micros();
-            println!("{n}");
+            // println!("{n}");
             let n = n as f64;
             let n = n.tan().sin();
             let n = n * 100000000.0;
-            let n = n.abs();
+            // let n = n.abs();
             let n = format!("{:.0}", n);
-            println!("{n}");
+            // println!("{n}");
+            out = n.trim().parse().unwrap();
         }
         Err(_) => panic!("SystemTime before UNIX EPOCH!"),
     }
+    out
 }
