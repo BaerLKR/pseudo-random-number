17ac4ddd3e5615591ab2b8cc4a643f3f9385b39f
Lovis Rentsch <gpg@lovirent.eu>
2023-06-05 23:15:49 +0200

    kinda works

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,13 @@ fn main() {
 
     match SystemTime::now().duration_since(UNIX_EPOCH) {
         Ok(n) => {
-            let n = n.as_secs();
+            let n = n.as_micros();
+            println!("{n}");
             let n = n as f64;
-            let n = n.sin().cos();
+            let n = n.tan().sin();
+            let n = n * 100000000.0;
+            let n = n.abs();
+            let n = format!("{:.0}", n);
             println!("{n}");
         }
         Err(_) => panic!("SystemTime before UNIX EPOCH!"),
