9e04c053e73abfea8422e92d859341e6d0775e35
Lovis Rentsch <gpg@lovirent.eu>
2023-06-05 23:38:52 +0200

    added range value

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,21 @@
 fn main() {
-    println!("{}", pseudorandom())
+    println!("{}", pseudorandom(100))
 }
 const PI: f64 = std::f64::consts::PI;
-fn pseudorandom() -> i32 {
+fn pseudorandom(max: i32) -> i32 {
     let out: i32;
     use std::time::{SystemTime, UNIX_EPOCH};
     match SystemTime::now().duration_since(UNIX_EPOCH) {
         Ok(n) => {
             let n = n.as_micros() as f64;
-            // let n = format!("{:.0}", n.tan().sin() * 100000000.0);
-            let n = format!("{:.0}", custom_sin(n, 340763470.0) * 1000000000.0);
+            let n = custom_sin(n, 3428975112.0, max);
             out = n.trim().parse().unwrap();
         }
         Err(_) => panic!("SystemTime before UNIX EPOCH!"),
     }
     out
 }
-fn custom_sin(x: f64, frequency: f64) -> f64 {
-    (frequency * x * 2.0 * PI).sin()
+fn custom_sin(x: f64, frequency: f64, max: i32) -> String {
+    let c = max as f64 * (frequency * x * 2.0 * PI).sin();
+    format!("{:.0}", c.abs())
 }
