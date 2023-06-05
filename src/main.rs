2a8d6513b741535f3286034e7b5be6cf42decaf1
Lovis Rentsch <gpg@lovirent.eu>
2023-06-05 23:44:09 +0200

    compressed it down

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,18 @@
 fn main() {
     println!("{}", pseudorandom(100))
 }
-const PI: f64 = std::f64::consts::PI;
 fn pseudorandom(max: i32) -> i32 {
-    let out: i32;
-    use std::time::{SystemTime, UNIX_EPOCH};
-    match SystemTime::now().duration_since(UNIX_EPOCH) {
-        Ok(n) => {
-            let n = n.as_micros() as f64;
-            let n = custom_sin(n, 3428975112.0, max);
-            out = n.trim().parse().unwrap();
-        }
-        Err(_) => panic!("SystemTime before UNIX EPOCH!"),
-    }
+    let out = match std::time::SystemTime::now().duration_since(std::time::UNIX_EPOCH) {
+        Ok(n) => format!(
+            "{:.0}",
+            max as f64
+                * (32957239857.0 * n.as_micros() as f64 * 2.0 * std::f64::consts::PI)
+                    .sin()
+                    .abs()
+        )
+        .parse()
+        .expect("Error making the random number to a number"),
+        Err(e) => panic!("SystemTime before UNIX EPOCH! ({e})"),
+    };
     out
-}
-fn custom_sin(x: f64, frequency: f64, max: i32) -> String {
-    let c = max as f64 * (frequency * x * 2.0 * PI).sin();
-    format!("{:.0}", c.abs())
 }
