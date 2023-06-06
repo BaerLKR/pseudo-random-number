39acecd038748b9ed42649a786a8515255ee2b53
Lovis Rentsch <gpg@lovirent.eu>
2023-06-06 06:59:02 +0200

    working upper and lower range

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,15 @@
 fn main() {
-    println!("{}", pseudorandom(100))
+    println!("{}", pseudorandom([-100, 0]))
 }
-fn pseudorandom(max: i32) -> i32 {
+fn pseudorandom(minmax: [i32;2]) -> i32 {
+    let offs = (minmax[0] as f64 + minmax[1] as f64) * 0.5;
+    let add = minmax[0] as f64 - offs;
     let out = match std::time::SystemTime::now().duration_since(std::time::UNIX_EPOCH) {
         Ok(n) => format!(
             "{:.0}",
-            max as f64
+            add 
                 * (32957239857.0 * n.as_micros() as f64 * 2.0 * std::f64::consts::PI)
-                    .sin()
-                    .abs()
+                    .sin() + offs
         )
         .parse()
         .expect("Error making the random number to a number"),
